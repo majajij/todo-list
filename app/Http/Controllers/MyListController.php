67b87e954e3d7aa44391fc3547c5489be94ef658
EL MEHDI AIT FAKIR <elmehdiaitfakir@gmail.com>
2022-10-07 21:19:21 +0100

    verify authentication user in controller lists

--- app/Http/Controllers/MyListController.php
+++ app/Http/Controllers/MyListController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\MyList;
 use Illuminate\Http\Request;
+use Exception;
 
 class MyListController extends Controller
 {
@@ -14,7 +15,10 @@ class MyListController extends Controller
      */
     public function index()
     {
-        $my_lists = MyList::paginate(3);
+        $my_lists = auth()
+            ->user()
+            ->myLists()
+            ->paginate(3);
         return response()->json(['data' => $my_lists], 200);
     }
 
@@ -28,13 +32,14 @@ class MyListController extends Controller
     {
         try {
             $new_list = MyList::create([
+                'user_id' => auth()->user()->id,
                 'task' => $request->task,
             ]);
         } catch (\Throwable $th) {
             return response()->json(['data' => ['message' => $th->getMessage()]], 400);
         }
 
-        return response()->json(['data' => $new_lists], 200);
+        return response()->json(['data' => $new_list], 200);
     }
 
     /**
@@ -43,9 +48,19 @@ class MyListController extends Controller
      * @param  \App\Models\MyList  $myList
      * @return \Illuminate\Http\Response
      */
-    public function show(MyList $myList)
+    public function show($id)
     {
-        return response()->json(['data' => $myList], 200);
+        try {
+            $my_list = MyList::where('id', $id)
+                ->where('user_id', auth()->user()->id)
+                ->first();
+            if (!$my_list) {
+                throw new Exception('There is no List with this ID');
+            }
+        } catch (\Throwable $th) {
+            return response()->json(['data' => ['message' => $th->getMessage()]], 400);
+        }
+        return response()->json(['data' => $my_list], 200);
     }
 
     /**
@@ -55,19 +70,24 @@ class MyListController extends Controller
      * @param  \App\Models\MyList  $myList
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, MyList $myList)
+    public function update(Request $request, $id)
     {
         try {
-            $myList->update([
-                'task' => $request->task,
-            ]);
+            $my_list = MyList::where('id', $id)
+                ->where('user_id', auth()->user()->id)
+                ->first();
+            if (!$my_list) {
+                throw new Exception('Update failed, please verify the ID list');
+            } else {
+                $my_list->update([
+                    'task' => $request->task,
+                ]);
+            }
         } catch (\Throwable $th) {
-            //TODO change class of 404
-            // verify status code
-            return response()->json(['data' => ['message' => $th->getStatus()]], 400);
+            return response()->json(['data' => ['message' => $th->getMessage()]], 400);
         }
 
-        return response()->json(['data' => $myList], 200);
+        return response()->json(['data' => $my_list], 200);
     }
 
     /**
@@ -76,16 +96,21 @@ class MyListController extends Controller
      * @param  \App\Models\MyList  $myList
      * @return \Illuminate\Http\Response
      */
-    public function destroy(MyList $myList)
+    public function destroy($id)
     {
         try {
-            $myList->delete();
+            $my_list = MyList::where('id', $id)
+                ->where('user_id', auth()->user()->id)
+                ->first();
+            if (!$my_list) {
+                throw new Exception('Delete failed, please verify the ID list');
+            } else {
+                $my_list->delete();
+            }
         } catch (\Throwable $th) {
-            //TODO change class of 404
-            // verify status code
-            return response()->json(['data' => ['message' => $th->getStatus()]], 400);
+            return response()->json(['data' => ['message' => $th->getMessage()]], 400);
         }
 
-        return response()->json(['data' => $myList], 200);
+        return response()->json(['data' => $my_list], 200);
     }
 }
--- database/migrations/2022_10_05_185824_create_my_lists_table.php
+++ database/migrations/2022_10_05_185824_create_my_lists_table.php
@@ -14,7 +14,10 @@ return new class extends Migration {
     {
         Schema::create('my_lists', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table
+                ->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
             $table->string('task');
             $table->timestamps();
         });
